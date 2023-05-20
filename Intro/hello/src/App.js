263a1c9c27c0a8f83118e3b92bb8c3937d165926
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-05-20 16:58:23 +0100

    Fundamentos e noçoes basicas em react

---
+++ Intro/hello/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>meu primeiro projecto em react</h1>
+          <p>noçoes basicas</p>
+        
+    </div>
+  );
+}
+
+export default App;
+
+
+
